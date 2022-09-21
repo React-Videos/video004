f4db98aced947f728c27b7750c9814bcc2777300
Fernando Warno <ferwar@hotmail.com>
2022-09-21 05:48:44 -0300

    css - video007

--- video004/src/App.js
+++ video004/src/App.js
@@ -7,9 +7,10 @@ import { CreateTodoButton } from './CreateTodoButton';
 // import './App.css';
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
+  { text: 'Escribir...', completed: false },
 ];
 
 function App() {
@@ -21,7 +22,11 @@ function App() {
       
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text}/>
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed}
+            />
         ))}
       </TodoList>
       <CreateTodoButton />
